763f3d86d26ec9eb8eae362a3790d7a4965dbd95
Andrew Martin <andrewlmartin@yahoo.co.uk>
2026-03-05 08:17:24 +0000

    Well that was painful, but it's a start

---
+++ cognition/feedback/translations/translation_en_US.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>success_feedback/behavior.xar://behavior_layer1/keyframe1/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Well done!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Well done!</translation>
+        </message>
+    </context>
+    <context>
+        <name>success_feedback/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>success_feedback/behavior.xar:/success/Celebrate/Celebrate/Say</name>
+        <message>
+            <location filename="success_feedback/behavior.xar" line="0"/>
+            <source>That's right!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">That's right!</translation>
+        </message>
+    </context>
+    <context>
+        <name>success_feedback/behavior.xar:/success/behavior_layer1/keyframe1/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>That's right!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">That's right!</translation>
+        </message>
+    </context>
+    <context>
+        <name>success_feedback/behavior.xar:/success/behavior_layer1/test/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+</TS>
